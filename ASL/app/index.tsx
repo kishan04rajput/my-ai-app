c37445af1ed346803c194d30163824c4ee08649b
kishan04rajput <kishan9rajput@gmail.com>
2025-06-07 21:14:12 +0530

    # Refactor Index component to improve layout and keyboard handling
    ASL/app/index.tsx
    - Wrapped the existing content in a KeyboardAvoidingView to enhance user experience when the keyboard is displayed, preventing content from being obscured.
    - Maintained the existing structure with ScrollView and StatusBar for consistent layout and visibility.

--- ASL/app/index.tsx
+++ ASL/app/index.tsx
@@ -1,4 +1,9 @@
-import { ScrollView, StatusBar, View } from "react-native";
+import {
+    KeyboardAvoidingView,
+    ScrollView,
+    StatusBar,
+    View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Section from "../components/Section";
 
@@ -8,20 +13,25 @@ export default function Index() {
             edges={["top", "left", "right", "bottom"]}
             style={{ flex: 1 }}
         >
-            <StatusBar backgroundColor="black" barStyle="dark-content" />
-            <ScrollView style={{ flex: 1 }}>
-                <View
-                    style={{
-                        flex: 1,
-                        width: "100%",
-                        alignItems: "center",
-                    }}
-                >
-                    <Section title="ASSETS" backgroundColor="#D4EDDA" />
-                    <Section title="SAVINGS" backgroundColor="#CCE5FF" />
-                    <Section title="LIABILITIES" backgroundColor="#F8D7DA" />
-                </View>
-            </ScrollView>
+            <KeyboardAvoidingView behavior="padding" style={{ flex: 1 }}>
+                <StatusBar backgroundColor="black" barStyle="dark-content" />
+                <ScrollView style={{ flex: 1 }}>
+                    <View
+                        style={{
+                            flex: 1,
+                            width: "100%",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Section title="ASSETS" backgroundColor="#D4EDDA" />
+                        <Section title="SAVINGS" backgroundColor="#CCE5FF" />
+                        <Section
+                            title="LIABILITIES"
+                            backgroundColor="#F8D7DA"
+                        />
+                    </View>
+                </ScrollView>
+            </KeyboardAvoidingView>
         </SafeAreaView>
     );
 }
