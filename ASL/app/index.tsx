e919a4b269b9ca014f2233d3556c01bb95e1fd17
kishan04rajput <kishan9rajput@gmail.com>
2025-06-07 20:58:12 +0530

    # Refactor Index component to enhance layout and user experience
    ASL/app/index.tsx
    - Replaced the View component with SafeAreaView to ensure proper layout on different devices.
    - Added ScrollView to allow for scrolling when content exceeds the screen height.
    - Updated the StatusBar to have a black background and dark content for better visibility.
    - Adjusted the inner View component to align items centrally and ensure full width usage.

--- ASL/app/index.tsx
+++ ASL/app/index.tsx
@@ -1,18 +1,27 @@
-import { View } from "react-native";
+import { ScrollView, StatusBar, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import Section from "../components/Section";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Section title="ASSETS" backgroundColor="#D4EDDA" />
-      <Section title="SAVINGS" backgroundColor="#CCE5FF" />
-      <Section title="LIABILITIES" backgroundColor="#F8D7DA" />
-    </View>
-  );
+    return (
+        <SafeAreaView
+            edges={["top", "left", "right", "bottom"]}
+            style={{ flex: 1 }}
+        >
+            <StatusBar backgroundColor="black" barStyle="dark-content" />
+            <ScrollView style={{ flex: 1 }}>
+                <View
+                    style={{
+                        flex: 1,
+                        width: "100%",
+                        alignItems: "center",
+                    }}
+                >
+                    <Section title="ASSETS" backgroundColor="#D4EDDA" />
+                    <Section title="SAVINGS" backgroundColor="#CCE5FF" />
+                    <Section title="LIABILITIES" backgroundColor="#F8D7DA" />
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    );
 }
